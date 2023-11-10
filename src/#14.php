de1e910c4c12754992af45d73bb14ad8c81c835e
Meiki <mickey.0103.eiking@outlook.jp>
2023-11-10 12:17:28 +0900

    Create #14.php

---
+++ src/#14.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>小説概要入力画面</title>
+    </head>
+    <body>
+        <h2>小説を作成する</h2>
+        <p>タイトル</p>
+        <input type="text" name="name"><br>
+        <p>概要</p>
+        <input type="text" name="discription"><br>
+        <div class="money">
+            <p>価格</p>
+            <input type="number" name="price">
+        </div>
+    </body>
+</html>
